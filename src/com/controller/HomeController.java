cb9d2317a97289a9d3ba7c02a2f691728f3239e3
mohininerkar <mohininerkar22@gmail.com>
2023-04-30 08:36:50 +0530

    intitially project add

---
+++ src/com/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Controller
+public class HomeController 
+{
+//InternalResourceViewResolver
+//DispatcherServlet
+	
+	@RequestMapping(value ="/hello")
+public ModelAndView hello()
+{
+	System.out.println("hello controller call");
+	return new ModelAndView("success");
+}
+}
